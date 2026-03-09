b0a45af0cfc220943be1e20647cf1be861ba4ca1
Sejun Moon <msj990305@gmail.com>
2026-03-09 16:07:25 -0500

    Encoder

---
+++ wheelchair_lowlevel/encoder_esp32/src/main.cpp
@@ -0,0 +1,48 @@
+#include <Arduino.h>
+
+// GPIO
+const int ENCODER_A = 4;
+const int ENCODER_B = 6;
+
+volatile long motorPosition = 0; 
+
+void IRAM_ATTR updateMotorPosition(){
+    if(digitalRead(ENCODER_B) != digitalRead(ENCODER_A)){
+        // forward
+        motorPosition++;
+    }
+    else{
+        // backward
+        motorPosition--;
+    }
+}
+
+void setup(){
+    Serial.begin(115200);
+
+    pinMode(ENCODER_A, INPUT);
+    pinMode(ENCODER_B, INPUT);
+
+    attachInterrupt(digitalPinToInterrupt(ENCODER_A), updateMotorPosition, CHANGE);
+
+
+}
+
+void loop(){
+    static long last_count = 0;
+    static unsigned long last_time = millis();
+
+    if(millis() - last_time >= 1000){
+        long current = motorPosition;
+        long diff = current - last_count;
+
+        Serial.print("Count: ");
+        Serial.print(current);
+
+        Serial.print("  Delta: ");
+        Serial.println(diff);
+
+        last_count = current;
+        last_time = millis();
+    }
+}
